794f31e4a2817fdd00affad86054913dfd02b758
Oleksandr Makogin <sashamakohin@gmail.com>
2023-01-10 12:49:41 +0200

    color switcher, timer, promise generator

--- src/js/01-color-switcher.js
+++ src/js/01-color-switcher.js
@@ -1,1 +1,30 @@
+const startBtn = document.querySelector('[data-start]');
+const stopBtn = document.querySelector('[data-stop]');
+const body = document.querySelector('body');
 
+let timerId = null;
+
+startBtn.addEventListener('click', () => {
+  timerId = setInterval(onStartView, 1000);
+});
+
+stopBtn.addEventListener('click', onStopView);
+
+function onStartView(e) {
+  startBtn.setAttribute('disabled', true);
+  stopBtn.removeAttribute('disabled');
+  body.style.backgroundColor = `${getRandomHexColor()}`;
+  console.log('hello from onStart');
+}
+
+function onStopView() {
+  startBtn.removeAttribute('disabled');
+  stopBtn.setAttribute('disabled', true);
+  startBtn.removeEventListener('click', onStartView);
+  clearInterval(timerId);
+  console.log('hello from onStop');
+}
+
+function getRandomHexColor() {
+  return `#${Math.floor(Math.random() * 16777215).toString(16)}`;
+}
--- src/js/03-promises.js
+++ src/js/03-promises.js
@@ -1,8 +1,40 @@
-function createPromise(position, delay) {
-  const shouldResolve = Math.random() > 0.3;
-  if (shouldResolve) {
-    // Fulfill
-  } else {
-    // Reject
+const refForm = document.querySelector('.form');
+refForm.addEventListener('submit', onSubmitForm);
+
+function onSubmitForm(e) {
+  e.preventDefault();
+
+  const {
+    elements: { delay, step, amount },
+  } = e.currentTarget;
+
+  const delayStep = Number(step.value);
+  const quantity = Number(amount.value);
+  let firstDelay = Number(delay.value);
+
+  for (let position = 1; position <= quantity; position += 1) {
+    const delay = (firstDelay += position === 1 ? 0 : delayStep);
+
+    createPromise({ position, delay })
+      .then(({ position, delay }) => {
+        console.log(`✅ Fulfilled promise ${position} in ${delay}ms`);
+      })
+      .catch(({ position, delay }) => {
+        console.log(`❌ Rejected promise ${position} in ${delay}ms`);
+      });
   }
+}
+
+function createPromise({ position, delay }) {
+  const shouldResolve = Math.random() > 0.3;
+
+  return new Promise((resolve, reject) => {
+    setTimeout(() => {
+      if (shouldResolve) {
+        resolve({ position, delay });
+      } else {
+        reject({ position, delay });
+      }
+    }, delay);
+  });
 }
